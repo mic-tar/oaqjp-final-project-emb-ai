c800a06835deb8cfcbd27eebcf953ccd22bbaf00
mic-tar <162429370+mic-tar@users.noreply.github.com>
2024-03-12 10:16:30 -0400

    feat: add emotion detector

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(URL, json=input_json, headers=header)
+    formated_response = json.loads(response.text)
+
+    emotions = formated_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+
+    if response.status_code == 200:
+        return emotions
+    elif response.status_code > 400:
+        print('Error: ', response.status_code)
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None}
